7c1f7faa05933263886d106d502517ae0fa77f9a
timlyo <timlyomaddison@gmail.com>
2014-06-10 13:40:07 +0100

    started work on a gui system
     Changes to be committed:
    	modified:   gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,21 @@
+import pygame
+
 class Gui:
-	def __init__(self,window):
+	def __init__(self):
+		self.mainFont = pygame.font.Font(None, 24)
+		self.elements = {}
+
+		self.createElements()
 		print ("created gui")
+
+	def draw(self, surface):
+		for item in self.elements:
+			surface.blit(self.elements[item].surface,(0,0))
+
+	def createElements(self):
+		self.elements["fps"] = Widget("Hello World", self.mainFont, (0,0) )
+
+class Widget:
+	def __init__(self, text, font, position):
+		self.surface = font.render(text ,1 , (255,255,255))
+		self.position = position
